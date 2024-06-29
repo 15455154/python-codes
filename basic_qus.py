39d35a11766234b96d2ffd5407a9d335788b3e0e
MJ HACKER TAMIL <118051725+15455154@users.noreply.github.com>
2024-06-29 18:42:32 +0530

    Update and rename hi to basic_qus.py

---
+++ basic_qus.py
@@ -0,0 +1,17 @@
+ans = input("your school is VVS .   True or false: ")
+#cretes a variable in which a input from a user is stored
+ans = ans.lower()
+#makes the answer to smaller case
+if ans == "true" :
+    print("yes you are absolutely correct")
+#cheaks the answer and if true prints
+else:#if NOT true
+    print("Sorry your wrong please try agine")
+    ans = input("your school is VVS .   True or false: ")
+    ans = ans.lower() 
+    if ans == "true" :
+        # if answer is correct print yes you are absolutely correct
+     print("yes you are absolutely correct")
+     # if answer is wrong print Sorry your wrong please try again
+    else:
+     print("Sorry your wrong please try again")
